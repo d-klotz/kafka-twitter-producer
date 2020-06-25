9e750ad91b72ac9b707524309d2276d33a23ae1a
Daniel <klotzdan@gi-de.com>
2020-06-25 07:37:16 +0200

    added a producer to resend the tweets

--- src/main/java/com/klotz/kafka/twitter/Producer.java
+++ src/main/java/com/klotz/kafka/twitter/Producer.java
@@ -10,10 +10,13 @@ import com.twitter.hbc.core.endpoint.StatusesFilterEndpoint;
 import com.twitter.hbc.core.processor.StringDelimitedProcessor;
 import com.twitter.hbc.httpclient.auth.Authentication;
 import com.twitter.hbc.httpclient.auth.OAuth1;
+import org.apache.kafka.clients.producer.*;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.List;
+import java.util.Properties;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.TimeUnit;
@@ -29,7 +32,7 @@ public class Producer {
     private final String SECRET = "Your twitter secret here";
 
     //You can set whatever terms you want to stream
-    private final List<String> TRACKED_TERMS = Lists.newArrayList("corona");
+    private final List<String> TRACKED_TERMS = Lists.newArrayList("kafka");
 
     public static void main(String[] args) {
         new Producer().run();
@@ -40,6 +43,19 @@ public class Producer {
         BlockingQueue<String> msgQueue = new LinkedBlockingQueue<String>(1000);
         Client client = createTwitterClient(msgQueue);
         client.connect();
+
+        KafkaProducer<String, String> producer = createKafkaProducer();
+
+        //Exiting the application
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            logger.info("Stopping the Application...");
+            logger.info("Shutting down Twitter Client...");
+            client.stop();
+            logger.info("Closing Producer...");
+            producer.close();
+            logger.info("done.");
+        }));
+
 
         // on a different thread, or multiple different threads....
         while (!client.isDone()) {
@@ -53,17 +69,23 @@ public class Producer {
 
             if(msg != null){
                 logger.info(msg);
+                producer.send(new ProducerRecord<>("twitter_tweets", null, msg), new Callback() {
+                    @Override
+                    public void onCompletion(RecordMetadata recordMetadata, Exception e) {
+                        if(e != null) {
+                            logger.error("Something bad happened", e);
+                        }
+                    }
+                });
             }
         }
-        logger.info("End of application");
     }
 
     public Client createTwitterClient(BlockingQueue<String> msgQueue) {
         // Declare the host you want to connect to, the endpoint, and authentication (basic auth or oauth) */
         Hosts hosebirdHosts = new HttpHosts(Constants.STREAM_HOST);
-        StatusesFilterEndpoint hosebirdEndpoint = new StatusesFilterEndpoint();
-        List<String> terms = TRACKED_TERMS;
-        hosebirdEndpoint.trackTerms(terms);
+        StatusesFilterEndpoint hosebirdEndpoint = new StatusesFilterEndpoint();;
+        hosebirdEndpoint.trackTerms(TRACKED_TERMS);
 
         // These secrets should be read from a config file
         Authentication hosebirdAuth = new OAuth1(CONSUMER_KEY, CONSUMER_SECRET,
@@ -77,5 +99,15 @@ public class Producer {
                 .processor(new StringDelimitedProcessor(msgQueue));                         // optional: use this if you want to process client events
 
         return builder.build();
+    }
+
+    public KafkaProducer<String, String> createKafkaProducer() {
+        Properties properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        KafkaProducer<String, String> producer = new KafkaProducer<String, String>(properties);
+        return producer;
     }
 }
